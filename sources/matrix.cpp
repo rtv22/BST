9974412633b497135e4cbc544a56b731c5b8cd5c
Bozey98 <dmusatov98@gmail.com>
2017-03-20 09:00:26 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,15 +1,94 @@
 #include "matrix.hpp"
 
-matrix_t::matrix_t() noexcept : rows_{0}, columns_{0}, elements_{nullptr}
-{
+Matrix::Matrix(int length) {
+	row = length;
+	col = length;
+
+	mas = new int*[row];
+	for (int i = 0; i < row; i++) {
+		mas[i] = new int[col];
+		for (int j = 0; j < col; j++) {
+			mas[i][j] = 0;
+		}
+	}
 }
 
-auto matrix_t::rows() -> unsigned int
-{
-    return rows_;
+Matrix::Matrix(int r, int c) {
+	row = r;
+	col = c;
+
+	mas = new int*[row];
+	for (int i = 0; i < row; i++) {
+		mas[i] = new int[col];
+	}
 }
 
-auto matrix_t::columns() -> unsigned int
-{
-    return columns_;
+Matrix::~Matrix() {
+	for (int i = 0; i < row; i++) {
+		delete[] mas[i];
+	}
+
+	delete[] mas;
+}
+
+Matrix::Matrix(const Matrix&a) {
+	row = a.row;
+	col = a.col;
+
+	mas = new int*[row];
+	for (int i = 0; i < row; i++) {
+		mas[i] = new int[col];
+		for (int j = 0; j < col; j++) {
+			mas[i][j] = a.mas[i][j];
+		}
+	}
+}
+
+void Matrix::fill( const char*file) {
+	ifstream fin1(file);
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			fin1 >> mas[i][j];
+		}
+	}
+}
+
+void Matrix::show() const {
+	
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			cout << mas[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+Matrix Matrix::operator+(const Matrix& a) const {
+
+	Matrix help(row, col);
+
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			help.mas[i][j] = mas[i][j] + a.mas[i][j];
+		}
+	}
+	return help;
+}
+
+Matrix Matrix::operator*(const Matrix& a) const {
+
+	Matrix help(row, col);
+
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			help.mas[i][j] = 0;
+			for (int k = 0; k < col; k++) {
+				help.mas[i][j] += mas[i][k] * a.mas[k][j];
+			}
+		}
+	}
+	return help;
 }
