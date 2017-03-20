2007e20ddbfc579a59bea4a941d61fba792490c6
Bozey98 <dmusatov98@gmail.com>
2017-03-20 09:18:31 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,3 +6,9 @@ SCENARIO("matrix init without parametrs", "[init wp]") {
 	REQUIRE(matrix.rows() == 4);
 	REQUIRE(matrix.columns() == 4);
 }
+
+SCENARIO("matrix init with parametrs", "[init withp]") {
+	Matrix matrix(6,6);
+	REQUIRE(matrix.rows() == 6);
+	REQUIRE(matrix.columns() == 6);
+}
