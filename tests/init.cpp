3719dde14c01287476ead530fc28e82c08e473b5
Bozey98 <dmusatov98@gmail.com>
2017-03-20 09:03:59 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,7 +1,7 @@
 #include <matrix.hpp>
 #include <catch.hpp>
 
-SCENARIO("matrix init", "[init]") {
+SCENARIO("matrix init without parametrs", "[init wp]") {
 	matrix_t matrix;
 	REQUIRE(matrix.rows() == 0);
 	REQUIRE(matrix.columns() == 0);
