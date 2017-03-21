b42b7c1c04906e433ebfe7ab535c6d3b6b2df74b
Bozey98 <dmusatov98@gmail.com>
2017-03-21 13:16:28 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -22,4 +22,6 @@ public:
 	Matrix operator*(const Matrix&) const;
 	bool operator==(const Matrix&) const;
 	Matrix operator=(const Matrix& x) const;
+	friend istream& operator >> (istream& is, const Matrix& m) const;
+	friend ostream& operator << (std::ostream&, const Matrix&) const;
 };
